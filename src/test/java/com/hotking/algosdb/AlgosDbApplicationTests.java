f38d01bdf67fa95405fd28bb037efafdd82151cf
Hotking228 <kraulov@bk.ru>
2026-03-26 16:35:15 +0300

    Initial commit

---
+++ src/test/java/com/hotking/algosdb/AlgosDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotking.algosdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgosDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
