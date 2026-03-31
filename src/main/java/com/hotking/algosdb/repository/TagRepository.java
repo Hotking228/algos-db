5694add14fd3402e1a53983fdd598753668c116d
Hotking228 <kraulov@bk.ru>
2026-03-31 11:27:26 +0300

    Добавлен метод поиска тега по алгоритму

--- src/main/java/com/hotking/algosdb/repository/TagRepository.java
+++ src/main/java/com/hotking/algosdb/repository/TagRepository.java
@@ -3,10 +3,21 @@ package com.hotking.algosdb.repository;
 import com.hotking.algosdb.entity.Tag;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface TagRepository extends JpaRepository<Tag, Integer> {
 
     List<Tag> findByName(String name);
+
+    @Query(value = "SELECT\n" +
+            "    t.id,\n" +
+            "    t.name\n" +
+            "FROM tag t\n" +
+            "JOIN algo_tag a_t ON t.id = a_t.tag_id\n" +
+            "JOIN algorithm a ON a_t.algo_id = a.id\n" +
+            "WHERE a.id = :algoId;",
+    nativeQuery = true)
+    List<Tag> findByAlgoId(Integer algoId);
 }
